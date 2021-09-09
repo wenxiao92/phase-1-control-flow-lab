3e1bf806e404855350ff34cf21f82118ab325f51
Wen Xiao <wennstyle@gmail.com>
2021-09-08 23:57:11 -0400

    commit tree e49076f0ca9ee5072f361f0f9f3f81d4586ab68a

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
+  if (someNumber < 400 ) {
+    return 'This one is on me!';
+  } else if (2000 < someNumber && someNumber < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (2501 <= someNumber) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let destination = (city === 'NYC' ? "Ok, sounds good." : "No go.");
+
+  return destination;
 }
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(tipAmount){
   // Write your code here!
+
+  let response;
+
+  switch (tipAmount) {
+    case 'generous': response = "Thank you so much.";
+      break;
+    case 'not as generous': response = "Thank you.";
+      break;
+
+    default:
+      response = "Bye.";
+      break;
+
+  }
+  return response;
 }
